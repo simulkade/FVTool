46e8e7d5a715ea6e519fe367a31b28727af9020e
AAE <e.eftekhari@gmail.com>
2015-09-04 09:59:29 +0200

    more utility functions are functional, explicit diffusion tutorial works now

--- Examples/Tutorial/diffusiontutorialExplicit.m
+++ Examples/Tutorial/diffusiontutorialExplicit.m
@@ -6,31 +6,32 @@ clear
 
 %% define the domain
 L = 5;  % domain length
-Nx = 1000; % number of cells
-meshstruct = buildMesh1D(Nx, L);
+Nx = 100; % number of cells
+meshstruct = createMesh1D(Nx, L);
 BC = createBC(meshstruct); % all Neumann boundary condition structure
-BC.left.a = 0; BC.left.b=1; BC.left.c=1; % left boundary
-BC.right.a = 0; BC.right.b=1; BC.right.c=0; % right boundary
+BC.left.a(:) = 0; BC.left.b(:)=1; BC.left.c(:)=1; % left boundary
+BC.right.a(:) = 0; BC.right.b(:)=1; BC.right.c(:)=0; % right boundary
 x = meshstruct.cellcenters.x;
 %% define the transfer coeffs
 D_val = 1;
 alfa = createCellVariable(meshstruct, 1);
 Dave = createFaceVariable(meshstruct, D_val);
 %% define initial values
-c.Old = createCellVariable(meshstruct, 0); % initial values
-c.value = c.Old;
+c_old = createCellVariable(meshstruct, 0, BC); % initial values
+c = c_old;
 %% loop
 dt = 0.00001; % time step
 final_t = 100;
 for t=dt:dt:final_t
-    % step 1: calculate ghost cell values
-    ctemp = cellBoundary(meshstruct, BC, c.Old);
-    % step 2: calculate divergence term
-    RHS = divergenceTerm(meshstruct, Dave.*gradientTerm(meshstruct, ctemp));
-    % step 3: calculate the new value for internal cells
-    c.value = dt*excludeGhostRHS(meshstruct, RHS+sourceExplicitTerm(meshstruct, c.Old))+c.Old(:);
+    % step 1: calculate divergence term
+    RHS = divergenceTerm(Dave.*gradientTerm(c_old));
+    % step 2: calculate the new value for internal cells
+    c_old_int=internalCells(c_old);
+    c_val_int = dt*excludeGhostRHS(meshstruct, RHS+constantSourceTerm(c_old))+c_old_int(:);
     c_analytical = 1-erf(x/(2*sqrt(D_val*t)));
-    figure(1);plot(x, c.value, x, c_analytical, '--');
-    c.Old = c.value;
+    figure(1);plot(x, c_val_int, x, c_analytical, 'r--');drawnow;
+    % Step 3: calculate the ghost cell values
+    c.value=cellBoundary(c_val_int, BC);
+    c_old = c;
     disp(t)
 end
--- Utilities/excludeGhostRHS.m
+++ Utilities/excludeGhostRHS.m
@@ -1,58 +1,64 @@
-function RHSout = excludeGhostRHS(MeshStructure, RHS)
+function RHSout = excludeGhostRHS(MS, RHS)
 % this function cuts out the RHS values related to ghost cells and returns
 % an RHS, which is only for internal cells
-% 
+%
 % SYNOPSIS:
-%   
-% 
+%
+%
 % PARAMETERS:
-%   
-% 
+%
+%
 % RETURNS:
-%   
-% 
+%
+%
 % EXAMPLE:
-% 
+%
 % SEE ALSO:
-%     
+%
 
 %{
 Copyright (c) 2012, 2013, Ali Akbar Eftekhari
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or 
-without modification, are permitted provided that the following 
+Redistribution and use in source and binary forms, with or
+without modification, are permitted provided that the following
 conditions are met:
 
-    *   Redistributions of source code must retain the above copyright notice, 
+    *   Redistributions of source code must retain the above copyright notice,
         this list of conditions and the following disclaimer.
-    *   Redistributions in binary form must reproduce the above 
-        copyright notice, this list of conditions and the following 
-        disclaimer in the documentation and/or other materials provided 
+    *   Redistributions in binary form must reproduce the above
+        copyright notice, this list of conditions and the following
+        disclaimer in the documentation and/or other materials provided
         with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %}
 
 % check the size of the variable and the mesh dimension
-d = MeshStructure.dimension;
-N = MeshStructure.numberofcells;
-G = MeshStructure.numbering;
+d = MS.dimension;
 
 if (d ==1) || (d==1.5)
-	RHSout = RHS(reshape(G(2:end-1),N,1));
+	Nx=MS.dims(1);
+	G = 1:Nx+2;
+	RHSout = RHS(reshape(G(2:end-1),Nx,1));
 elseif (d == 2) || (d == 2.5) || (d==2.8)
-	RHSout = RHS(reshape(G(2:end-1,2:end-1),N(1)*N(2),1));
+	Nxy = MS.dims;
+	Nx = Nxy(1); Ny = Nxy(2);
+	G=reshape(1:(Nx+2)*(Ny+2), Nx+2, Ny+2);
+	RHSout = RHS(reshape(G(2:end-1,2:end-1),Nx*Ny,1));
 elseif (d == 3) || (d==3.2)
-    RHSout = RHS(reshape(G(2:end-1,2:end-1,2:end-1),N(1)*N(2)*N(3),1));
+	Nxyz = MS.dims;
+	Nx = Nxyz(1); Ny = Nxyz(2); Nz = Nxyz(3);
+	G=reshape(1:(Nx+2)*(Ny+2)*(Nz+2), Nx+2, Ny+2, Nz+2);
+  RHSout = RHS(reshape(G(2:end-1,2:end-1,2:end-1),Nx*Ny*Nz,1));
 end
