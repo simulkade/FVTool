edac6b7ea99192596bb1bf87150dda08741e8dce
Ali Akbar Eftekhari <aliak@win.dtu.dk>
2016-05-25 16:52:24 +0200

    tiny bug fix and imbibition example

--- MeshGeneration/cellVolume.m
+++ MeshGeneration/cellVolume.m
@@ -34,9 +34,9 @@ switch dim
     case 2
         c=meshvar.cellsize.x(2:end-1)*meshvar.cellsize.y(2:end-1)';
     case 2.5 % cylindrical
-        c=2.0*pi()*meshvar.cellcenters.x.*m.cellsize.x(2:end-1)*m.cellsize.y(2:end-1)';
+        c=2.0*pi()*meshvar.cellcenters.x.*meshvar.cellsize.x(2:end-1)*meshvar.cellsize.y(2:end-1)';
     case 2.8 % radial
-        c=meshvar.cellcenters.x.*m.cellsize.x(2:end-1)*m.cellsize.y(2:end-1)';
+        c=meshvar.cellcenters.x.*meshvar.cellsize.x(2:end-1)*meshvar.cellsize.y(2:end-1)';
     case 3
         error('Not available yet');
     case 3.2
