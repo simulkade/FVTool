d712af59f4e1f4aa7de38be706e444ac3bddd684
Ali A Eftekhari <e.eftekhari@gmail.com>
2018-11-15 13:51:26 +0100

    cellVolume now works for 3D domains as well
    Out of my own need!

--- MeshGeneration/cellVolume.m
+++ MeshGeneration/cellVolume.m
@@ -38,8 +38,24 @@ switch dim
     case 2.8 % radial
         c=meshvar.cellcenters.x.*meshvar.cellsize.x(2:end-1)*meshvar.cellsize.y(2:end-1)';
     case 3
-        error('Not available yet');
+        Nx = meshvar.dims(1);
+        Ny = meshvar.dims(2);
+        Nz = meshvar.dims(3);
+        DXp = repmat(meshvar.cellsize.x(2:end-1), 1, Ny, Nz);
+        DYp = repmat(meshvar.cellsize.y(2:end-1)', Nx, 1, Nz);
+        DZ = zeros(1,1,Nz+2);
+        DZ(1,1,:) = meshvar.cellsize.z;
+        DZp=repmat(DZ(1,1,2:end-1), Nx, Ny, 1);
+        c=DXp.*DYp.*DZp;
     case 3.2
-        error('Not available yet');
+        N = meshvar.dims;
+        Nr = N(1); Ntetta=N(2); Nz = N(3);
+        rp = repmat(meshvar.cellcenters.x, 1, Ntetta, Nz);
+        DRp = repmat(meshvar.cellsize.x(2:end-1), 1, Ntetta, Nz);
+        DTHETAp = repmat(meshvar.cellsize.y(2:end-1)', Nr, 1, Nz);
+        DZ = zeros(1,1,Nz+2);
+        DZ(1,1,:) = meshvar.cellsize.z;
+        DZp=repmat(DZ(1,1,2:end-1), Nr, Ntetta, 1);
+        c=rp.*DRp.*DTHETAp.*DZp;
 end
 cellvol= createCellVariable(meshvar, c, BC);
