8230f12297ce68127cdfc1bb05e9847c38818649
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-06-27 15:24:25 +0200

    Create pc_simple.m

---
+++ FieldGeology/pc_simple.m
@@ -0,0 +1,10 @@
+function res = pc(pc_w, pc_nw, n_w, n_nw, swc, snwc)
+%PC returns a simple imbibition Pc curve
+% pc_w_max = 10.0*pc_w;
+% pc_nw_max = 10.0*pc_nw;
+% res=((sw<=swc).* ...
+%     + ...
+%     + ...
+%     krw0.*sws.^nw+(sw>1-sor).*(-(1-krw0)./sor.*(1.0-sw)+1.0));
+end
+
