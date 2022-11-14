5db580f8bc616957d84ae1e442491befa55f96ee
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-11-14 14:53:40 +0100

    Update rough_fracture.m

--- FieldGeology/rough_fracture.m
+++ FieldGeology/rough_fracture.m
@@ -2,6 +2,8 @@ function d=rough_fracture(Nx,Ny, d_avg, sd, clx, cly, sd_cut_coef)
 % All credits go to http://www.mysimlabs.com/surface_generation.html
 % d_avg [m] average aperture between two rough surfaces of the fracture
 % sd [m] standard deviation
+% sd_cut_coef is a multiplier for the standard deviation that specifies an
+% upper bound for  the surface roughness values. The lower bound is zero.
 Lx=1.0; % domain length
 x=linspace(-Lx/2.0,Lx/2.0,Nx);
 y=linspace(-Lx/2.0,Lx/2.0,Ny);
