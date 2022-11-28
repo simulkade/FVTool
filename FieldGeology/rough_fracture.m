f01fd989a65a8c706fa62e4f0c1a814fc3528382
Ali A Eftekhari <e.eftekhari@gmail.com>
2022-11-28 15:22:18 +0100

    random seed
    will find a more general solution later

--- FieldGeology/rough_fracture.m
+++ FieldGeology/rough_fracture.m
@@ -1,13 +1,15 @@
-function d=rough_fracture(Nx,Ny, d_avg, sd, clx, cly, sd_cut_coef)
+function d=rough_fracture(Nx,Ny, d_avg, sd, clx, cly, sd_cut_coef, rng_val)
 % All credits go to http://www.mysimlabs.com/surface_generation.html
 % d_avg [m] average aperture between two rough surfaces of the fracture
 % sd [m] standard deviation
 % sd_cut_coef is a multiplier for the standard deviation that specifies an
 % upper bound for  the surface roughness values. The lower bound is zero.
+% rng_val is a seed for generating random number
 Lx=1.0; % domain length
 x=linspace(-Lx/2.0,Lx/2.0,Nx);
 y=linspace(-Lx/2.0,Lx/2.0,Ny);
 [X,Y]=ndgrid(x,y);
+rng(rng_val);
 Z=sd*randn(Nx,Ny); % normal distribution
 F = exp(-(X.^2/(clx*clx/2.0)+Y.^2/(cly*cly/2.0)));
 % Gaussian filter
